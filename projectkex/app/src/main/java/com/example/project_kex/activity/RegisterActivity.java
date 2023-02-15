bdb82c53d70b0a30978517c7a260c35ea3fdff0b
Ruiminmrm <maruimin1998@gmail.com>
2023-02-15 14:25:12 +0100

    login and register sites

---
+++ projectkex/app/src/main/java/com/example/project_kex/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_kex.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project_kex.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
